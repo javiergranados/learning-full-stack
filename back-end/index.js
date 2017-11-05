db59316b6291f78edbaec099a24be30a76c9ffd1
jvgranados <jgranadosflores@gmail.com>
2017-11-05 01:06:11 +0100

    StandardJS added into back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict'
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.listen(port, () => {
-    console.log(`API REST corriendo en http://localhost:${port}`);
-});
+  console.log(`API REST corriendo en http://localhost:${port}`)
+})
