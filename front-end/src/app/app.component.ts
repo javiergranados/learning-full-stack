2c05e65e99f8a1e53be1d4aea9bf00d63724f986
jvgranados <jgranadosflores@gmail.com>
2017-11-12 20:12:06 +0100

    Method "signUp" added

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Constructor
+  constructor(private http: Http) {}
+
   //Init
   title = "App Login";
   user = "Usuario";
@@ -23,5 +30,25 @@ export class AppComponent {
   //Actions
   logIn(){
     //TODO
+  }
+
+  signUp(){
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    let options = new RequestOptions({ headers: headers });
+
+    const body = {
+      name : this.credentials.user,
+      password : this.credentials.password
+    };
+
+    this.http
+      .post('http://localhost:3000/api/login', body, options)
+      .map(response => response.json())
+      .subscribe(
+        response =>  alert('OK' + response.message),
+        error => alert('ERROR' + error.message)
+    );
   }
 }
